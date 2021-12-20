6599cd8fdb47c4aeec3d0976a6ce24deea8251dc
Max Schallwig <max@mobalyticshq.com>
2021-12-20 21:02:37 +0100

    25Decorators

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
-class Animal:
-    friends = []
+def response_to_approacher(name, approaching=True):
+    def inner_response(func):
+        def wrapper(*args, **kwargs):
+            if approaching is True:
+                print(f"A {name} is coming")
+            else:
+                print(f"A {name} is leaving")
+            response = func(*args, **kwargs)
+            return response
 
-    def __init__(self, height=100, weight=100):
-        self.height = height
-        self.weight = weight
-        self._private_weight = 50
-        self.__very_private_weight = 30
-
-    def print_height(self):
-        print(f"This animal's height is: {self.get_height()}")
-
-    def get_height(self):
-        return self.height
-
-    def get_fur_colour(self):
-        return self.fur_colour
-
-    def get_friends(self):
-        return self.friends
-
-    def set_height(self, height):
-        self.height = height
-
-    def greet(self):
-        print("This animal makes no sound")
+        return wrapper
+    return inner_response
 
 
-class Dog(Animal):
-    def __init__(self, height, weight, fur_colour):
-        self.fur_colour = fur_colour
-        super().__init__(height=height, weight=weight)
-
-    @classmethod
-    def greet(self):
-        print("Woof, woof")
+def print_hello(func):
+    def wrapper(*args, **kwargs):
+        print("hello")
+        return func(*args, **kwargs)
+    return wrapper
 
 
-class Config:
-    RATE_LIMIT = ...
-    HOST_URL = ...
+@response_to_approacher("milkman", False)
+@print_hello
+def conjure_sound(sound):
+    return sound * 2
 
 
-sample_dog = Dog(50, 25, "brown")
-print(sample_dog.fur_colour, sample_dog.height)
-sample_dog.print_height()
-sample_dog.greet()
-
-Dog.greet()
-print("A dog's starting friends are:", Dog.height)
+return_value = conjure_sound("woof")
+print("return_value:", return_value)
