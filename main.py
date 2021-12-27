83e897c42b9e744021d0c673ee2c075ca42a4ea0
Max Schallwig <max@mobalyticshq.com>
2021-12-27 11:45:01 +0100

    29ResponseModels

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse, PlainTextResponse
-
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/test", response_class=JSONResponse)
+class ProfileInfo(BaseModel):
+    short_description: str
+    long_bio: str
+
+
+class User(BaseModel):
+    username: str
+    profile_info: ProfileInfo
+    liked_posts: Optional[list[int]] = None
+
+
+def get_user_info() -> User:
+    profile_info = {
+        "short_description": "My bio description",
+        "long_bio": "This is our longer bio"
+    }
+
+    profile_info = ProfileInfo(**profile_info)
+    user_content = {
+        "username": "ourusername",
+        "liked_posts": [1],
+        "profile_info": profile_info
+    }
+
+    return User(**user_content)
+
+
+@app.get("/user/me", response_model=User)
 def test_endpoint():
-    return {"test key": "some value", "another key": 'another value', 4: "some more values", 5: 4,
-            "nested key": {"some internal key": "some internal value"}}
 
+    user = get_user_info()
 
-@app.get("/", response_class=PlainTextResponse)
-def home():
-    return "welcome"
+    return user
