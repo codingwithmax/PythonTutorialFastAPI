2683d3f162d1a186524a551ee1665e07ddb02901
Max Schallwig <max@mobalyticshq.com>
2021-12-28 12:54:18 +0100

    32RequestBodies

--- main.py
+++ main.py
@@ -22,29 +22,30 @@ class FullUserProfile(User):
     long_bio: str
 
 
-def get_user_info(user_id: str = "default") -> FullUserProfile:
-    profile_infos = {
-        "default": {
-            "short_description": "My bio description",
-            "long_bio": "This is our longer bio"
-        },
-        "user_1": {
-            "short_description": "User 1's bio description",
-            "long_bio": "User 1's longer bio"
-        }
+class CreateUserResponse(BaseModel):
+    user_id: int
+
+
+profile_infos = {
+    0: {
+        "short_description": "My bio description",
+        "long_bio": "This is our longer bio"
     }
+}
+
+users_content = {
+    0: {
+        "liked_posts": [1] * 9,
+    }
+}
+
+
+def get_user_info(user_id: int = 0) -> FullUserProfile:
+
     profile_info = profile_infos[user_id]
-    users_content = {
-        "default": {
-            "liked_posts": [1] * 9,
-            "profile_info": profile_info
-        },
-        "user_1": {
-            "liked_posts": [],
-            "profile_info": profile_info
-        }
-    }
+
     user_content = users_content[user_id]
+
     user = User(**user_content)
 
     full_user_profile = {
@@ -55,6 +56,30 @@ def get_user_info(user_id: str = "default") -> FullUserProfile:
     return FullUserProfile(**full_user_profile)
 
 
+def create_user(full_profile_info: FullUserProfile) -> int:
+    global profile_infos
+    global users_content
+
+    new_user_id = len(profile_infos)
+    liked_posts = full_profile_info.liked_posts
+    short_description = full_profile_info.short_description
+    long_bio = full_profile_info.long_bio
+
+    print("before:")
+    print("users_content:", users_content)
+    print("profile_infos:", profile_infos)
+
+    users_content[new_user_id] = {"liked_posts": liked_posts}
+    profile_infos[new_user_id] = {
+        "short_description": short_description,
+        "long_bio": long_bio
+    }
+    print("after:")
+    print("users_content:", users_content)
+    print("profile_infos:", profile_infos)
+    return new_user_id
+
+
 @app.get("/user/me", response_model=FullUserProfile)
 def test_endpoint():
     full_user_profile = get_user_info()
@@ -63,8 +88,15 @@ def test_endpoint():
 
 
 @app.get("/user/{user_id}", response_model=FullUserProfile)
-def get_user_by_id(user_id: str):
-    
+def get_user_by_id(user_id: int):
+
     full_user_profile = get_user_info(user_id)
 
     return full_user_profile
+
+
+@app.post("/users", response_model=CreateUserResponse)
+def add_user(full_profile_info: FullUserProfile):
+    user_id = create_user(full_profile_info)
+    created_user = CreateUserResponse(user_id=user_id)
+    return created_user
