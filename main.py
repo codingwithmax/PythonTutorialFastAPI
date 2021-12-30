d65eafe68784d2835c35d84738d50844bd477eab
Max Schallwig <max@mobalyticshq.com>
2021-12-30 14:36:41 +0100

    34PutDelete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -77,28 +78,31 @@ def get_all_users_with_pagination(start: int, limit: int) -> (list[FullUserProfi
     return list_of_users, total
 
 
-def create_user(full_profile_info: FullUserProfile) -> int:
+def create_update_user(full_profile_info: FullUserProfile, new_user_id: Optional[int] = None) -> int:
     global profile_infos
     global users_content
 
-    new_user_id = len(profile_infos)
+    if new_user_id is None:
+        new_user_id = len(profile_infos)
     liked_posts = full_profile_info.liked_posts
     short_description = full_profile_info.short_description
     long_bio = full_profile_info.long_bio
-
-    print("before:")
-    print("users_content:", users_content)
-    print("profile_infos:", profile_infos)
 
     users_content[new_user_id] = {"liked_posts": liked_posts}
     profile_infos[new_user_id] = {
         "short_description": short_description,
         "long_bio": long_bio
     }
-    print("after:")
-    print("users_content:", users_content)
-    print("profile_infos:", profile_infos)
+
     return new_user_id
+
+
+def delete_user(user_id: int) -> None:
+    global profile_infos
+    global users_content
+
+    del profile_infos[user_id]
+    del users_content[user_id]
 
 
 @app.get("/user/me", response_model=FullUserProfile)
@@ -116,6 +120,17 @@ def get_user_by_id(user_id: int):
     return full_user_profile
 
 
+@app.put("/user/{user_id}")
+def update_user(user_id: int, full_profile_info: FullUserProfile):
+    create_update_user(full_profile_info, user_id)
+    return None
+
+
+@app.delete("/user/{user_id}")
+def remove_user(user_id: int):
+    delete_user(user_id)
+
+
 @app.get("/users", response_model=MultipleUsersResponse)
 def get_all_users_paginated(start: int = 0, limit: int = 2):
     users, total = get_all_users_with_pagination(start, limit)
@@ -125,6 +140,6 @@ def get_all_users_paginated(start: int = 0, limit: int = 2):
 
 @app.post("/users", response_model=CreateUserResponse)
 def add_user(full_profile_info: FullUserProfile):
-    user_id = create_user(full_profile_info)
+    user_id = create_update_user(full_profile_info)
     created_user = CreateUserResponse(user_id=user_id)
     return created_user
