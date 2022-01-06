4180545892072515cb952bc35984ffa2c383fdb1
Max Schallwig <max@mobalyticshq.com>
2022-01-06 09:03:37 +0100

    43LoggingFormatting

--- app/routes/user.py
+++ app/routes/user.py
@@ -9,8 +9,15 @@ import logging
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename="log.txt")
+logging.basicConfig(
+    format="%(levelname)-6s %(name)-15s %(asctime)s.%(msecs)03d %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="log.txt",
+)
 logger.setLevel(logging.WARNING)  # debug -> info -> warning -> error -> critical
+
+console = logging.StreamHandler()
+logger.addHandler(console)
 
 
 def create_user_router() -> APIRouter:
