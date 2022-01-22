17acad08f7640caf22eec1875d80ca187ff0eac8
Max Schallwig <max@mobalyticshq.com>
2022-01-22 11:44:29 +0100

    55TestCoverage

--- sample_requests/sync_requests.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def get_and_parse_user(base_url: str, endpoint_prefix: str, user_id: int):
-    url = base_url + endpoint_prefix + str(user_id)
-    response = requests.get(url)
-    post_response = requests.post(url)
-    return post_response.json()
