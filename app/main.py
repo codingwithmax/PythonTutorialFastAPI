d21fd691806afee666d14c56923e409196527a35
Max Schallwig <max@mobalyticshq.com>
2022-02-19 13:09:42 +0100

    67UpdatingOurApplicationToReadFromTheDatabase

--- app/main.py
+++ app/main.py
@@ -1,33 +1,22 @@
 from fastapi import FastAPI
 from app.routes.user import create_user_router
 from app.exeption_handlers import add_exception_handlers
+from app.clients.db import DatabaseClient
+from app.config import Config
 
 
 def create_application() -> FastAPI:
-    profile_infos, users_content = create_profile_infos_and_users_content()
-    user_router = create_user_router(profile_infos, users_content)
+
+    config = Config()
+    tables = ["user", "liked_post"]
+    database_client = DatabaseClient(config, tables)
+    user_router = create_user_router(database_client)
 
     app = FastAPI()
     app.include_router(user_router)
     add_exception_handlers(app)
     
     return app
-
-
-def create_profile_infos_and_users_content():
-    profile_infos = {
-        0: {
-            "short_description": "My bio description",
-            "long_bio": "This is our longer bio"
-        }
-    }
-
-    users_content = {
-        0: {
-            "liked_posts": [1] * 9,
-        }
-    }
-    return profile_infos, users_content
 
 
 app = create_application()
